bfebde9a9b6d814f0e3477266c06016cde7a6096
joensumin <soomis7@naver.com>
2022-02-15 15:46:42 +0900

    데이터 처리하기

--- SearchDaumBlog/Presentation/MainViewController/MainViewController.swift
+++ SearchDaumBlog/Presentation/MainViewController/MainViewController.swift
@@ -31,13 +31,90 @@ class MainViewController: UIViewController {
     }
     
     private func bind(){
-        /// Rx코드
-        /// ui 바인딩 작업
+        let blogResult = searchView.shouldLoadResult
+            .flatMapLatest { query in
+                SearchBlogNetwork().searchBlog(query: query)
+            }
+            .share()
+        
+        let blogValue = blogResult
+            .compactMap { data -> DKBlog? in
+                guard case .success(let value ) = data else {
+                    return nil
+                }
+                return value
+            }
+        
+        let blogError = blogResult
+            .compactMap { data -> String? in
+                guard case .failure(let error) = data else {
+                    return nil
+                }
+                return error.localizedDescription
+            }
+        
+        //네트워크를 통해 가져온 값을 Celldata로 변환
+        let cellData = blogValue
+            .map { blog -> [BlogListCellData] in
+                return blog.documents
+                    .map { doc in
+                        let thumbnailURL = URL(string: doc.thumbnail ?? "" )
+                        return BlogListCellData(thumnailURL: thumbnailURL,
+                                                name: doc.name,
+                                                title: doc.title,
+                                                dateTime: doc.datetime)
+                    }
+            }
+    
+        //FilterView를 선택했을때 표시되는 alertSheet Type설정
+        let sortedType = alertActionTapped
+            .filter {
+                switch $0 {
+                case .title, .datetime:
+                    return true
+                default:
+                    return false
+                }
+            }
+            .startWith(.title)
+       
+        //MainViewController -> ListView
+        Observable.combineLatest(sortedType,cellData) {type, data -> [BlogListCellData] in
+            switch type {
+            case .title:
+                return data.sorted { $0.title ?? "" < $1.title ?? ""}
+            case .datetime:
+                return data.sorted { $0.dateTime ?? Date() > $1.dateTime ?? Date() }
+            default:
+                return data
+            }
+        }
+        .bind(to: listView.cellData)
+        .disposed(by: disposeBag)
         
         let alertSheetForSorting = listView.headerView.sortButtonTapped
             .map { _ -> Alert in
                 return (title:nil, message:nil, actions:[.title,.datetime,.cancel],style:.actionSheet)
             }
+        
+        let alertForErrorMessage = blogError
+            .map { message -> Alert in
+                return (
+                    title: "앗!",
+                    message: "예상치 못한 오류가 발생했습니다. 잠시후 다시 시도해 주세요. \(message)",
+                    actions: [.confirm],
+                    style: .alert)
+            }
+        
+        Observable
+            .merge(alertSheetForSorting, alertForErrorMessage)
+            .asSignal(onErrorSignalWith: .empty())
+            .flatMapLatest { alert -> Signal<AlertAction> in
+                let alertController = UIAlertController(title: alert.title, message: alert.message, preferredStyle: alert.style)
+                return self.presentAlertController(alertController, actions: alert.actions)
+            }
+            .emit(to: alertActionTapped)
+            .disposed(by: disposeBag)
             
         alertSheetForSorting
             .asSignal(onErrorSignalWith: .empty())
